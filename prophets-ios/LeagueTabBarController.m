1526c9ad64dbfd82050afc06fa9e32e6ca40f1a2
Ben Roesch <bcroesch@gmail.com>
2012-11-13 09:06:38 -0600

    More customizing of tab bar

--- prophets-ios/LeagueTabBarController.m
+++ prophets-ios/LeagueTabBarController.m
@@ -10,6 +10,7 @@
 #import "UIBarButtonItem+Additions.h"
 #import "League.h"
 #import "Membership.h"
+#import "UIColor+Additions.h"
 
 @interface LeagueTabBarController ()
 
@@ -27,6 +28,36 @@
     [self.navigationItem setLeftBarButtonItems:@[item] animated:YES];
     
     [self.tabBar setBackgroundImage:[UIImage imageNamed:@"leaguemenu_bgnd.png"]];
+    
+    for(UITabBarItem *item in self.tabBar.items){
+        UIImage *selected = nil;
+        UIImage *unselected = nil;
+        if ([item.title isEqualToString:@"Questions"]) {
+            selected = [UIImage imageNamed:@"leaguemenu_allquestions_pressed.png"];
+            unselected = [UIImage imageNamed:@"leaguemenu_allquestions.png"];
+        }
+        else if ([item.title isEqualToString:@"My Bets"]) {
+            selected = [UIImage imageNamed:@"leaguemenu_mybets.png"];
+            unselected = [UIImage imageNamed:@"leaguemenu_mybets.png"];
+        }
+        else if ([item.title isEqualToString:@"League Stats"]) {
+            selected = [UIImage imageNamed:@"leaguemenu_stats_pressed.png"];
+            unselected = [UIImage imageNamed:@"leaguemenu_stats.png"];
+        }
+        else if ([item.title isEqualToString:@"Club Room"]) {
+            selected = [UIImage imageNamed:@"leaguemenu_clubroom_pressed.png"];
+            unselected = [UIImage imageNamed:@"leaguemenu_clubroom.png"];
+        }
+        else if ([item.title isEqualToString:@"League Settings"]) {
+            selected = [UIImage imageNamed:@"leaguemenu_settings_pressed.png"];
+            unselected = [UIImage imageNamed:@"leaguemenu_settings.png"];
+        }
+        [item setFinishedSelectedImage:selected withFinishedUnselectedImage:unselected];
+        [item setTitleTextAttributes:@{
+            UITextAttributeTextColor : [UIColor creamColor]
+         }
+                            forState:UIControlStateNormal];
+    }
     
     for(UIViewController *vc in self.viewControllers){        
         if([vc respondsToSelector:@selector(setMembership:)]){
