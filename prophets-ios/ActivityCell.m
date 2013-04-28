5f32cd3340fea6ca3508f6fcb1606f1109181689
Ben Roesch <bcroesch@gmail.com>
2013-04-28 15:25:12 -0500

    Fix label positioning on activity cells

--- prophets-ios/ActivityCell.m
+++ prophets-ios/ActivityCell.m
@@ -17,23 +17,20 @@
 -(void)setActivity:(Activity *)activity{
     _activity = activity;
     self.contentLabel.text = activity.content;
-    self.contentLabel.frame = RectWithNewHeight([Utilities heightForString:activity.content
-                                                                  withFont:self.contentLabel.font
-                                                                     width:self.contentLabel.frame.size.width], self.contentLabel.frame);
+    CGFloat height = [Utilities heightForString:activity.content
+                                       withFont:self.contentLabel.font
+                                          width:self.contentLabel.frame.size.width];
     
-    self.contentLabel.center = CGPointMake(self.contentLabel.center.x, self.contentView.center.y);
+    self.contentLabel.frame = RectWithNewHeight(height, self.contentLabel.frame);
     
     self.commentsLabel.text = [NSString stringWithFormat:@"%@", activity.commentsCount];
-    
-    self.commentsLabel.center = CGPointMake(self.commentsLabel.center.x, self.contentLabel.center.y-1);
-    self.commentsImageView.center = CGPointMake(self.commentsImageView.center.x, self.contentLabel.center.y+2);
 }
 
 -(CGFloat)heightForCellWithActivity:(Activity *)activity{
     CGFloat contentHeight = [Utilities heightForString:activity.content
                                               withFont:self.contentLabel.font
                                                  width:self.contentLabel.frame.size.width];
-    return contentHeight + 20;
+    return contentHeight + 16;
 }
 
 
